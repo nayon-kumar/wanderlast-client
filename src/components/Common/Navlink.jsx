810fe78c4efbfb53de9790478b2ddcdfa15f6a53
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-13 10:14:16 +0600

    Navbar close issue fixed

--- src/components/Common/Navlink.jsx
+++ src/components/Common/Navlink.jsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-const Navlink = ({ children, href, className }) => {
+const Navlink = ({ children, href, className, onClick }) => {
   const pathName = usePathname();
   const active = href === pathName;
+
   return (
     <Link
       href={href}
-      className={`font-medium ${className} ${active ? "text-[#15A1BF] border-b-2 border-[#15A1BF]" : "text-[#0C0B0B]"}`}
+      onClick={onClick}
+      className={`font-medium ${className} ${
+        active ? "text-[#15A1BF] border-b-2 border-[#15A1BF]" : "text-[#0C0B0B]"
+      }`}
     >
       {children}
     </Link>
