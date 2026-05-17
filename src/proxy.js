ab461436c1f283e28f7b988dfef181926014f979
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-17 21:54:01 +0600

    All proxy added

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
 export async function proxy(request) {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -13,5 +12,5 @@ export async function proxy(request) {
 }
 
 export const config = {
-  matcher: ["/bookings", "/admin"],
+  matcher: ["/bookings", "/admin", "/destinations/:path+", "/profile"],
 };
